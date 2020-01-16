99474daf3a903c98eb1ee84aac5dd789020b6fb9
jhoo08 <jamisonhs.08@gmail.com>
2020-01-16 07:08:07 -0800

    updates to homework 2
    added more code

--- Homework2/ContextSwitch/ContextSwitch/BSP/switch.s
+++ Homework2/ContextSwitch/ContextSwitch/BSP/switch.s
@@ -32,25 +32,25 @@ EXC_RETURN_THREAD_MODE EQU 0xFFFFFFF9
  */
 TaskSwitch
     // Disable interrupts
-    
+        CPSID I
     // Save registers R4-R11 using register SP
-    
+        PUSH {R4-R11}
     // Set up argument for procedure call by copying SP to R0
-    
+        MOV R0, SP
     // Call scheduler(uint_32 sp) to save the current SP and determine the next
     // value of currentSP. It takes its input argument sp from R0.
-    
+        BL scheduler
     // Load address of currentSP into R0
-    
+        LDR R0, =SP
     // Load the value of currentSP into SP
-    
+        LDR SP, [SP]
     // Load registers R4-R11 using SP
-    
+        PUSH {R4-R11}
     // Ensure that we return from exception Handler Mode to Thread Mode by 
     // loading the value EXC_RETURN_THREAD_MODE into LR
-    
+        LDR LR =EXC_RETURN_THREAD_MODE
     // Enable interrupts
-    
+        CPSID I
     // Branch using LR to return from exception while atomically restoring 
     // registers R0-R3, R12, LR, PC and xPSR
     BX LR
